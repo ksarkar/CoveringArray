5ccb587b7f1cc9c343961a8f94f21f5330353e2a
Kaushik Sarkar <ksarkar1@asu.edu>
2016-03-04 18:47:14 -0700

    added copyright notice to the source files.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Java implementation of a number of Covering Array construction algorithms. Diffe
 1. The density algorithm
 2. The biased-density algorithm
 3. Different algorithms from the two-stage algorithm family
+
+Copyright (C) 2013-2016 Kaushik Sarkar <ksarkar1@asu.edu>
--- src/edu/asu/ca/kaushik/algorithms/structures/Interaction.java
+++ src/edu/asu/ca/kaushik/algorithms/structures/Interaction.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2013-2016 Kaushik Sarkar <ksarkar1@asu.edu>
+ * 
+ * This file is part of CoveringArray project.
+ * 
+ * CoveringArray is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * CoveringArray is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with CoveringArray.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
 package edu.asu.ca.kaushik.algorithms.structures;
 
 
